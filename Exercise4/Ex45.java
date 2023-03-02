39302283877ca1fa312e5eed80aad05842756d19
Universeisu <111554684+Universeisu@users.noreply.github.com>
2023-03-02 08:45:40 +0700

    Add files via upload

---
+++ Exercise4/Ex45.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex45 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number1 :");
+        int w = scan.nextInt();
+        System.out.print("Enter Number2 :");
+        int l = scan.nextInt();
+        System.out.println("ความกว้าง = " + w);
+        System.out.println("ความยาว = " + l);
+        int area = w * l;
+        System.out.print(area);
+}
+}
